aad1f3297a8a451450b2e59e315d4ec89ff8d81e
Alexander Biryuk <alex.biryuk@gmail.com>
2023-04-10 15:41:54 +0300

    add little bugfix

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Yandex_Maps_API/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
